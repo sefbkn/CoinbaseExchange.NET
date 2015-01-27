ef7a8b157271b43e696c27d21942630d39df1dd3
Youssef Boukenken <sbkn256@gmail.com>
2015-01-27 00:56:40 -0500

    * Initial commit

--- CoinbaseExchange.NET/Endpoints/Account/Account.cs
+++ CoinbaseExchange.NET/Endpoints/Account/Account.cs
@@ -14,9 +14,6 @@ namespace CoinbaseExchange.NET.Endpoints.Account
         public decimal Available { get; set; }
         public string Currency { get; set; }
 
-        /*
-         * [{"id":"9645a3a4-2563-4ec4-ac79-d220ff661116","currency":"USD","balance":"0.0000000000000000","hold":"0","available":"0.0000000000000000","profile_id":"f1cb7ecb-263f-4334-9e30-4654ad932c4c"},{"id":"8b509a4a-1c7f-4493-874d-300fcdd77c48","currency":"BTC","balance":"0.1000000000000000","hold":"0","available":"0.1000000000000000","profile_id":"f1cb7ecb-263f-4334-9e30-4654ad932c4c"}]
-         */
         public static Account FromJToken(JToken jtoken)
         {
             var id = jtoken["id"].Value<string>();
